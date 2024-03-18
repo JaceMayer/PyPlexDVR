cfbcadd50a0c0e1d7d9c468a3285c8b14176fcff
Jace Mayer <davidallancrompton@outlook.com>
2024-03-18 12:59:43 +0000

    Begin move to a unified epgItem

--- channel.py
+++ channel.py
@@ -8,12 +8,14 @@ from epgItem import epgItem
 from datetime import datetime, timedelta
 from config import dvrConfig
 
+
 class channel:
-    
+
     def __init__(self, channelDef):
         self.id = channelDef["id"]
         self.name = channelDef["name"]
-        self.url = 'http://%s:%s/stream/%s' % (dvrConfig["Server"]['bindAddr'], dvrConfig["Server"]['bindPort'], self.id)
+        self.url = 'http://%s:%s/stream/%s' % (
+        dvrConfig["Server"]['bindAddr'], dvrConfig["Server"]['bindPort'], self.id)
         self.scanDir = channelDef["baseDir"]
         if "showDirs" in channelDef:
             self.scanPaths = channelDef["showDirs"]
@@ -26,12 +28,13 @@ class channel:
         self.buffer = []
         t = threading.Thread(target=self.runChannel, args=())
         t.start()
-        self.createEPGItems()
+        if dvrConfig["EPG"]["generate"]:
+            self.createEPGItems()
 
     def createEPGItems(self):
         time = datetime.now()
         for item in self.showPaths:
-            self.epgData[item] = epgItem(item)
+            self.epgData[item] = epgItem(item, self.scanDir)
             self.epgData[item].startTime = time.strftime("%Y%m%d%H%M%S")
             time += timedelta(minutes=math.ceil(self.epgData[item].length))
             self.epgData[item].endTime = time.strftime("%Y%m%d%H%M%S")
@@ -54,7 +57,7 @@ class channel:
             for file in os.listdir(path):
                 if file.startswith('.') or file.endswith(".part"): continue
                 if os.path.isfile('%s/%s' % (path, file)) and self.isScannedFileVideo(file):
-                    self.showPaths.append('%s/%s' %(path, file))
+                    self.showPaths.append('%s/%s' % (path, file))
                 else:
                     for seasonFile in os.listdir('%s/%s' % (path, file)):
                         if self.isScannedFileVideo(seasonFile):
@@ -85,18 +88,19 @@ class channel:
     def runChannel(self):
         print('Starting FFMPEG channel %s' % self.name)
         while True:
-            cmd = ["ffmpeg","-v", "error", "-re", "-i", self.getShow(), "-q:v", "15","-acodec", "mp3", "-vf", "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,setsar=1", "-f", "mpegts",  "-"]
+            cmd = ["ffmpeg", "-v", "error", "-re", "-i", self.getShow(), "-q:v", "15", "-acodec", "mp3", "-vf",
+                   "scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,setsar=1",
+                   "-f", "mpegts", "-"]
             try:
-                process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize =0)
+                process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=0)
             except Exception as e:
                 print("Error during FFMPEG execution:", e)
+                return
             line = process.stdout.read(1024)
             while True:
                 if line == b'':
-                     process.poll()
-                     if isinstance(process.returncode, int):
-                         break
+                    process.poll()
+                    if isinstance(process.returncode, int):
+                        break
                 for buffer in self.buffer: buffer.append(line)
                 line = process.stdout.read(1024)
-                
-            
--- epgItem.py
+++ epgItem.py
@@ -2,11 +2,12 @@ import tvdb_v4_official
 import moviepy.editor as mp
 from config import dvrConfig
 
-class epgItem:
 
-    def __init__(self, path):
+class epgItem:
+    def __init__(self, path, channelBaseDir):
         self.t = tvdb_v4_official.TVDB(dvrConfig["EPG"]["TMDBAPIKey"])
         self.path = path
+        self.baseDir = channelBaseDir
         self.title = ""
         self.desc = ""
         self.length = 0
@@ -15,39 +16,20 @@ class epgItem:
         self.getEPGData()
 
     def getEPGData(self):
-        if '/Volumes/Storage/TV' in self.path:
-            self.title = self.path.split('/Volumes/Storage/TV/')[1].split('/')[0]
-            self.title = self.title.encode('utf-8').strip().decode()
+        self.title = self.path.split(self.baseDir)[1].split('/')[0]
+        self.title = self.title.encode('utf-8').strip().decode()
+        try:
+            show = self.t.search(self.title)
+            self.desc = show[0]['overview']
+        except Exception as e:
             try:
+                self.title = self.title.split('(')[0][:-1]
                 show = self.t.search(self.title)
                 self.desc = show[0]['overview']
             except Exception as e:
-                try:
-                    self.title = self.title.split('(')[0][:-1]
-                    show = self.t.search(self.title)
-                    self.desc = show[0]['overview']
-                except Exception as e:
-                    print("Unable to find match for %s" % self.title)
-                    self.desc = self.title
-            self.length = self.getLength()
-        else:
-            self.path = self.path.replace('/Volumes/Storage/Movies/','')
-            firstbracket = self.path.find("(")
-            res = self.t.search(self.path[:firstbracket].replace('.', ' '))
-            if len(res) == 0:
-                dateLoc = self.path.find("20")
-                res = self.t.search(self.path[:dateLoc].replace('.', ' '))
-                if len(res) == 0:
-                    print("Giving up on matching %s" % self.path)
-                    self.title = "A movie"
-                    self.desc = "Movie"
-                    self.length = self.getLength()
-            print("%s matched to %s" % (self.path, res[0]["extended_title"]))
-            tvdbData = res[0]
-            self.title = tvdbData["extended_title"]
-            self.desc = tvdbData["overview"]
-            extData = self.t.get_movie(tvdbData["tvdb_id"])
-            self.length = extData["runtime"]
+                print("Unable to find match for %s" % self.title)
+                self.desc = self.title
+        self.length = self.getLength()
 
     def getLength(self):
         print("Getting length for path %s" % self.path)
