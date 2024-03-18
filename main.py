47710e7fda55a47e6fd64b11ef8016d224a9f37e
Jace Mayer <davidallancrompton@outlook.com>
2024-03-18 12:42:50 +0000

    main: Send int formatted channel ID to generator

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def stream(channel):
                     yield buffer.pop(0)
         except GeneratorExit:
             channelMap[channel].removeBuffer(buffer)
-    return Response(generate(channel), mimetype='video/MP2T')
+    return Response(generate(int(channel)), mimetype='video/MP2T')
 
 
 @app.route('/lineup_status.json')
