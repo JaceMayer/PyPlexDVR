5ae6d331be1f05ae04033a2fc27067d5a4b0a746
Jace Mayer <davidallancrompton@outlook.com>
2024-03-18 21:16:30 +0000

    Start work on EPG item caching

---
+++ epgCache.py
@@ -0,0 +1,31 @@
+import os
+import yaml
+
+
+class epgCache:
+    def __init__(self, showName):
+        self.showName = showName
+        if not os.path.isdir("cache"):
+            print("Creating base cache dir")
+            os.mkdir("cache")
+        self.cache = {}
+        self.cacheLoaded = False
+
+    def getItemFromCache(self, itemName):
+        if itemName in self.cache["items"]:
+            return self.cache["items"]
+        return None
+
+    def addItemToCache(self, itemName, length):
+        self.cache["items"][itemName] = length
+
+    def loadCacheIfExists(self):
+        if not os.path.isfile("cache/%s.yaml" % self.showName):
+            print("No cache for item %s" % self.showName)
+            return
+        with open("cache/%s.yaml" % self.showName, 'r') as stream:
+            self.cache = yaml.safe_load(stream)
+
+    def saveCacheToDisk(self):
+        with open("cache/%s.yaml", "w") as stream:
+            yaml.dump(self.cache, stream)
--- epgItem.py
+++ epgItem.py
@@ -1,6 +1,7 @@
 import tvdb_v4_official
 import moviepy.editor as mp
 from config import dvrConfig
+from epgCache import epgCache
 
 
 class epgItem:
@@ -21,18 +22,30 @@ class epgItem:
         if title == "":
             title = self.path.split(self.baseDir)[1][1:-3]
         self.title = title.encode('utf-8').strip().decode()
-        try:
-            show = self.t.search(self.title)
-            self.desc = show[0]['overview']
-        except Exception as e:
+        cache = epgCache(self.title)
+        cache.loadCacheIfExists()
+        if not cache.cacheLoaded or cache.getItemFromCache(self.path) is None:
             try:
-                self.title = self.title.split('(')[0][:-1]
                 show = self.t.search(self.title)
                 self.desc = show[0]['overview']
             except Exception as e:
-                print("Unable to find match for %s" % self.title)
-                self.desc = self.title
-        self.length = self.getLength()
+                try:
+                    self.title = self.title.split('(')[0][:-1]
+                    show = self.t.search(self.title)
+                    self.desc = show[0]['overview']
+                except Exception as e:
+                    print("Unable to find match for %s" % self.title)
+                    self.desc = self.title
+            self.length = self.getLength()
+            if not cache.cacheLoaded:
+                cache.cache["title"] = self.title
+                cache.cache["desc"] = self.desc
+            cache.addItemToCache(self.path, self.length)
+            cache.saveCacheToDisk()
+        else:
+            self.length = cache.getItemFromCache(self.path)
+            self.desc = cache.cache["desc"]
+            self.title = cache.cache["title"]
 
     def getLength(self):
         try:
