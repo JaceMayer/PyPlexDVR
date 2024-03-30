70947b2b464817017b9465be1390660145193988
Jace Mayer <davidallancrompton@outlook.com>
2024-03-30 09:57:51 +0000

    gunicorn: attempt to fix tempfs issues within kubernetes

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,12 @@
 from config import dvrConfig
+import os 
+
 workers = dvrConfig["Server"]["gunicornWorkers"]
 threads = dvrConfig["Server"]["gunicornThreads"]
 bind = '%s:%s' % (dvrConfig["Server"]["bindAddr"], dvrConfig["Server"]["bindPort"])
+
+if os.environ.get("KUBERNETES_SERVICE_HOST", None) is not None:
+    print("Gunicorn running in Kubernetes")
+    worker_tmp_dir = "/dev/shm"
+    timeout = 600
+
